a299710df2298b3ed69e5072c93965144ab02b51
mtopo <maxptop@gmail.com>
2024-09-15 20:59:19 -0400

    Initial Commit

---
+++ OpenCal/OpenCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenCalApp.swift
+//  OpenCal
+//
+//  Created by Max Topolsky on 9/15/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
